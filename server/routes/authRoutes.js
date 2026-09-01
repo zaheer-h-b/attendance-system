a6f403faa17718cba00d9d4dcdfc06dc379a849a
Zaheer <zaheerhbannigol8861@gmail.com>
2026-09-01 14:36:17 +0530

    Update authentication routes

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,9 +1,15 @@
 import express from 'express';
+
 import { register, login } from '../controllers/authController.js';
 
 const router = express.Router();
 
+router.get('/test', (req, res) => {
+  res.json({ message: 'Auth routes are working' });
+});
+
 router.post('/register', register);
+
 router.post('/login', login);
 
 export default router;
